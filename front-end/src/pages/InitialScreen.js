785f0d78817852cfc037c7a33f1347671a5d0f9b
Ozan Cinci <91191123+OzanCinci@users.noreply.github.com>
2022-11-12 15:31:15 +0300

    init landing page

--- front-end/src/pages/InitialScreen.js
+++ front-end/src/pages/InitialScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LoginSelector from './LoginSelector'
 import styled from 'styled-components';
 
@@ -10,14 +10,51 @@ const LoginWrapper = styled.div`
   box-shadow: 0 0 10px black;
   margin: auto;
   margin-top: 300px;
+  overflow: hidden;
 `
+
+const TempWrapperHasta = styled.div`
+    width: 100%;
+    height: 100%;
+    transform: ${props=>props.authType==="Hasta"?"":"translateX(-100%)"};
+    transition: all 0.5s ease-in-out;
+    cursor: pointer;
+`
+
+const TempWrapperDoktor = styled.div`
+    width: 100%;
+    height: 100%;
+    transform: ${props=>props.authType==="Doktor"?"translateX(-100%)":""};
+    transition: all 0.5s ease-in-out;
+`
+
+const LoginFormContainer = styled.div`
+    width: 200%;
+    height: 90%;
+    display: flex;
+    flex-direction: row;
+`
+
 function InitialScreen() {
+    const [authType,setAuthType] = useState("Hasta")
+
+    const setterFunction = (arg)=>{
+        setAuthType(arg);
+    }
+
   return (
-    <div>
         <LoginWrapper>
-            <LoginSelector/>
+            <LoginSelector authType={authType} setterFunction={setterFunction}/>
+            
+            <LoginFormContainer>
+                <TempWrapperHasta authType={authType}>
+                hasta giriş ekranı
+                </TempWrapperHasta>
+                <TempWrapperDoktor authType={authType}>
+                doktor giriş ekranı
+                </TempWrapperDoktor>
+            </LoginFormContainer>
         </LoginWrapper>
-    </div>
   )
 }
 
